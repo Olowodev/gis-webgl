30d31d4759a7f7eee235fbea6af4a9d603ce2347
Adebayo Olowofoyeku <adebayoolowofoyeku@gmail.com>
2023-03-02 20:58:23 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {google} from 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAnzlq9yYSHWTqiw3BHC_utiL60Adf5MCI&callback=initMap'
+import { useEffect } from 'react';
 
 function App() {
+
+  const initMap = () => {
+    const uluru = { lat: -25.344, lng: 131.031 }
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru
+    })
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map
+    })
+  }
+
+  useEffect(() => {
+    window.initMap = initMap
+  })
   return (
     <div className="App">
-      <header className="App-header">
+      <div id='map'></div>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +36,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
